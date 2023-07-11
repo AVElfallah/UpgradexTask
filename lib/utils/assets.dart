261205efd5ce10fef0cea477d1aeafa527d5b2f1
AVElfallah <abdohegag@gmail.com>
2023-07-11 20:59:33 +0300

    init flutter project

---
+++ lib/utils/assets.dart
@@ -0,0 +1,9 @@
+class ProjectAssets {
+  ProjectAssets._();
+  static final ProjectAssets _instance = ProjectAssets._();
+  factory ProjectAssets() => _instance;
+
+  //REVIEW - here you can find assets paths
+
+  static const String logo = 'assets/images/logo.png';
+}
